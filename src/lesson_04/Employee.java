409317ab904ad2788215530eba399a69df53fce8
Nikolai-Shcherbina <nikolai.v.shcherbina@gmail.com>
2020-08-07 10:01:33 +0300

    Lesson_04 task 1, 2

---
+++ src/lesson_04/Employee.java
@@ -0,0 +1,16 @@
+package lesson_04;
+
+public class Employee {
+    String surname;
+    int salary;
+    int age;
+
+    Employee(String surname, int salary, int age){
+        this.surname = surname;
+        this.salary = salary;
+        this.age = age;
+    }
+
+
+
+}
