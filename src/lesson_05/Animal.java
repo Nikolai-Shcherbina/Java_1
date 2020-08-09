faff6e400108fb1c0a70b01e22e625b6846f1427
Nikolai-Shcherbina <nikolai.v.shcherbina@gmail.com>
2020-08-09 16:35:44 +0300

    Lesson_05 - created class Cat, Dog, Animal. (constructor)

---
+++ src/lesson_05/Animal.java
@@ -0,0 +1,12 @@
+package lesson_05;
+
+public class Animal {
+    private String name;
+    private int age;
+
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
---
+++ src/lesson_05/Dog.java
@@ -0,0 +1,8 @@
+package lesson_05;
+
+public class Dog extends Animal{
+    Dog(String name, int age) {
+        super(name, age);
+    }
+
+}
