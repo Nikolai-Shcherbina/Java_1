6b02fd6f916f00c0f6132c8055b3d9ced269c1f3
Nikolai-Shcherbina <nikolai.v.shcherbina@gmail.com>
2020-08-18 12:46:03 +0300

    Lesson_07

---
+++ src/ru/geebrains/java1/lessons/lesson_07/XoGUI.java
@@ -0,0 +1,14 @@
+package ru.geebrains.java1.lessons.lesson_07;
+import javax.swing.*;
+import java.awt.*;
+
+//JavaFX
+//thymeleaf
+//VAADIN
+//...
+
+public class XoGUI {
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+}
